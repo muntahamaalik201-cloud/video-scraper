b8c59d535ea3f33cbb82b79ad82e236483c98471
muntahamaalik201-cloud <muntahamaalik201@gmail.com>
2026-08-27 09:16:56 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# --- Google Sheets ---
+SPREADSHEET_ID = '19Jp82OeDSk9PIIIENPyhfUohTdURx-QxS-KMt2fvx04' 
+WORKSHEET_NAME = 'Ad Scraper'                   
+CREDENTIALS_FILE = 'creds.json'             
+
+# --- Scraper settings ---
+HEADLESS = False      
+
+WAIT_TIMEOUT = 20     # Seconds to wait for the video to load after clicking play
